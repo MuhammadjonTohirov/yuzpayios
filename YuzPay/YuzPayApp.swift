18eb913d939dceac55f65a317da26088992f3613
Muhammadjon Tohirov <tokhirov.mukhammadjon@gmail.com>
2022-12-05 16:32:52 +0500

    Initial Commit

---
+++ YuzPay/YuzPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  YuzPayApp.swift
+//  YuzPay
+//
+//  Created by applebro on 05/12/22.
+//
+
+import SwiftUI
+
+@main
+struct YuzPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
